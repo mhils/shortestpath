7971ea377d8ddc113d9f1014e3e71868af87d4dd
Maximilian Hils <git@maximilianhils.com>
2015-10-26 18:39:23 +0100

    fix tests

--- tests/testthat/test.plot.R
+++ tests/testthat/test.plot.R
@@ -1,7 +1,7 @@
 context("plot.spgraph")
 
 test_that("a shortest path graph plots without errors", {
-  plot(makeShortestPathGraph(karate))
+  plot(makeShortestPathGraph(make_graph("Dodecahedron")))
 })
 
 test_that("a meaningful error is raised if the graph argument is not an spgraph", {
