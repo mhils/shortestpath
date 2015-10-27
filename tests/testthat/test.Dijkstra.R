660830be959e3242738464234e078f1074ffd979
Dennis Motzke <ralf.tini-89g>
2015-10-27 20:10:04 +0100

    Created Testfile
    tbd

---
+++ tests/testthat/test.Dijkstra.R
@@ -0,0 +1,10 @@
+context("Dijkstra")
+
+library(igraph)
+
+test_that("makeEdgeWeights is a valid default factory", {
+  #tbd
+  #edge.weights <- makeEdgeWeights(make_graph("Dodecahedron"))
+  #expect_equal(edge.weights, rep(1,30))
+})
+  
