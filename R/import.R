84f5bacc9c2c0047d2e21c0cf72a6365aab45d6d
Maximilian Hils <git@maximilianhils.com>
2015-10-27 17:05:40 +0100

    remove NOTE

--- R/import.R
+++ R/import.R
@@ -3,3 +3,6 @@
 #' @importFrom wesanderson wes_palette
 #' @importFrom pryr partial
 NULL
+
+# https://github.com/smbache/magrittr/issues/29
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
