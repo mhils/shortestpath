e3bac16c8414afafdc974637d3ac688ca56a0e3d
Maximilian Hils <git@maximilianhils.com>
2015-10-27 19:21:32 +0100

    remove pryr dependency

--- R/import.R
+++ R/import.R
@@ -1,8 +1,7 @@
 #' @import igraph
 #' @importFrom magrittr %<>%
 #' @importFrom wesanderson wes_palette
-#' @importFrom pryr partial
 NULL
 
 # https://github.com/smbache/magrittr/issues/29
-if (getRversion() >= "2.15.1") utils::globalVariables(c(".")) 
+if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
