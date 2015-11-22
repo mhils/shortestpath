d6c0960adc75771691d1321c257e81f354231a92
Maximilian Hils <git@maximilianhils.com>
2015-11-22 15:59:35 +0100

    fix tests

--- tests/testthat/test.spresults.R
+++ tests/testthat/test.spresults.R
@@ -3,7 +3,7 @@ context("spresults")
 test_that("the spresults constructor works as expected", {
     steps <- list(make_graph("Bull"))
     results <- spresults(steps)
-    expect_true(is.spresults(spgraph))
+    expect_true(is.spresults(results))
 })
 
 test_that("spresults$name gives the name of the last graph", {
