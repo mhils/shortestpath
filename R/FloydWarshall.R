ac4c2924e7a98ca9ddd0e57fa50fcf2ff406d8a9
Maximilian Hils <git@maximilianhils.com>
2015-11-22 12:56:50 +0100

    floy warshall wip

--- R/FloydWarshall.R
+++ R/FloydWarshall.R
@@ -1,58 +1,63 @@
 library(igraph)
 
 #' Calculates the shortest path of all pairs of vertices in a graph
-#' 
-#' 
+#'
+#'
 #' @param graph The \code{igraph} object.
-#' @return A list of \code{spgraph} objects. Each \code{spgraph} object contains information about 
-#' a certain step in the optimization process representing by its attributes 
+#' @return A list of \code{spgraph} objects. Each \code{spgraph} object contains information about
+#' a certain step in the optimization process representing by its attributes
 #' @import igraph
-#' 
+#'
 floydWarshall = function(graph)
 {
   graph <- makeShortestPathGraph(graph, FALSE,FALSE)
   nVertices = length(V(graph))
-  
-  #Create the adjacency matrix with weighted edges  
+
+  #Create the adjacency matrix with weighted edges
   #(Assumption: weight of edges has to be in the edge attribute "weight" of the given igraph)
   am = get.adjacency(graph,attr='weight',sparse=FALSE)
-  
+
   #Update modyfied attributes
   graph$min_dists <- replace(graph$min_dists,which(am > 0),am[which(am > 0)])
   graph$shortest_path_predecessors <- replace(graph$shortest_path_predecessors,
                                                                 which(am > 0),
                                                                 which(am > 0, arr.ind = TRUE)[,2])
-  #initialization of a list which contains the spg graphs 
+
+  print(graph$shortest_path_predecessors)
+  #initialization of a list which contains the spg graphs
   result = list(graph)
   #Add all vertices one by one to the set of intermediate vertices.
   #---> Before start of a iteration, we have shortest distances between all pairs of vertices such that
   #the shortest distances consider only the vertices in set {0, 1, 2, .. k-1} as intermediate vertices.
-  #----> After the end of a iteration, vertex no. k is added to the set of intermediate vertices 
-  #and the set becomes {0, 1, 2, .. k} 
-    for(k in 1:nVertices){ 
+  #----> After the end of a iteration, vertex no. k is added to the set of intermediate vertices
+  #and the set becomes {0, 1, 2, .. k}
+    for(k in 1:nVertices){
       #Pick all vertices as source one by one
       for(i in 1:nVertices){
         #Pick all vertices as destination for the above picked source
         for(j in 1:nVertices){
-          #If vertex k is on the shortest path from
-          #i to j, then update the value of dist[i][j] 
-          if (graph$min_dists[i,k] + graph$min_dists[k,j] < graph$min_dists[i,j]){ 
-            graph$min_dists[i,j] = graph$min_dists[i,k] + graph$min_dists[k,j]
-            graph$shortest_path_predecessors[i,j] = k 
-          }else if(graph$min_dists[i,k] + graph$min_dists[k,j] == graph$min_dists[i,j]){
-            #überprüfen ob shortest_path_predecessor bereits in shortest_path_predecessor
-            if(!is.null (graph$shortest_path_predecessors[[i,j]]) &&
-               !(k %in% graph$shortest_path_predecessors[[i,j]]) &&
-                (k != i)
-               ){ 
-              graph$shortest_path_predecessors[[i,j]] =c(graph$shortest_path_predecessors[[i,j]],k)
+            if(i==k || j==k || i==j){
+                next
             }
-            
-          }
+            min_dist_current <- graph$min_dists[i,j]
+            min_dist_over_k <- graph$min_dists[i,k] + graph$min_dists[k,j]
+            if(min_dist_over_k < min_dist_current) {
+                graph$min_dists[i,j] <- min_dist_over_k
+                graph$shortest_path_predecessors[[i,j]] <- k
+
+                print(paste("# Set predecessor", "dist:", min_dist_over_k, "k:", k, "i:", i, "j:", j))
+
+            } else if (min_dist_over_k == min_dist_current){
+                if(!(k %in% graph$shortest_path_predecessors[[i,j]])){
+                    print(paste("# Add predecessor", "dist:", min_dist_over_k, "k:", k, "i:", i, "j:", j))
+                    graph$shortest_path_predecessors[[i,j]] <- c(graph$shortest_path_predecessors[[i,j]],k)
+                }
+            }
         }
       }
+      print(graph$shortest_path_predecessors)
       result = c(result,list(graph))
-    }  
+    }
 return(result)
 }
 
