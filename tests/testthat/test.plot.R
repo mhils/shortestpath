ea222396ab86c04928ace161e32d143ed9432db3
Maximilian Hils <git@maximilianhils.com>
2015-12-06 17:12:34 +0100

    tests++

--- tests/testthat/test.plot.R
+++ tests/testthat/test.plot.R
@@ -10,6 +10,21 @@ test_that("spresults are properly plotted", {
     plot(spresults)
 })
 
+test_that("manually specifying attributes works", {
+    spgraph <- as.spgraph(make_graph("Dodecahedron"))
+    plot(
+        spgraph,
+        vertex.color.by="manual",
+        vertex.color="#FAADBB",
+        vertex.frame.color.by="manual",
+        vertex.frame.color="#5E3443",
+        edge.color.by="manual",
+        edge.color = "#DE7192",
+        edge.label.color = "#D96788",
+        vertex.label.color	= "#DE7192"
+    )
+})
+
 test_that("a meaningful error is raised if the graph argument is not an spgraph", {
     expect_error(plot.spgraph("illegal"))
 })
