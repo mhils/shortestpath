e6bb78ad71f0749f6eb172613d90208caa57318d
Maximilian Hils <git@maximilianhils.com>
2015-12-16 11:15:31 +0100

    tests++

--- tests/testthat/test.toLatexGraph.R
+++ tests/testthat/test.toLatexGraph.R
@@ -4,8 +4,10 @@ test_that("toLatexGraph runs without errors", {
     graph <- randomGraph(n=20,euclidean=TRUE)
     a <- aStarSearch(graph,"A","K")
     fw <- floydWarshall(graph)
+    a2 <- aStarSearch(as.directed(graph), "A", "K")
 
     expect_output(toLatexGraph(a),"tikz")
     expect_output(toLatexGraph(fw),"tikz")
+    expect_output(toLatexGraph(a2),"tikz")
 })
 
